07466a5279023f0d28e89b6696add3aa56644e2a
TAdev0 <angles.tristan@protonmail.com>
2023-10-30 13:16:15 +0100

    updateTypeCastings

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -39,7 +39,7 @@ contract DeployMoodNft is Script {
         // would return ""
         string memory baseURL = "data:image/svg+xml;base64,";
         string memory svgBase64Encoded = Base64.encode(
-            bytes(string(abi.encodePacked(svg)))
+            bytes(string(abi.encodePacked(svg))) // Removing unnecessary type castings, this line can be resumed as follows : 'abi.encodePacked(svg)'
         );
         return string(abi.encodePacked(baseURL, svgBase64Encoded));
     }
